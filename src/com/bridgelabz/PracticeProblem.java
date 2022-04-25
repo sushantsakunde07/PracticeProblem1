626658551ff1d19ca47a336fedfc645bed41dc24
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 07:49:55 +0530

    Added: Largest element in array

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -1,33 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class PracticeProblem {
     public static void main(String[] args) {
-        //creating and initializing array
-        int arr[] = new int[]{1, 2, 8, 3, 2, 2, 2, 5, 1};
-        // Array frequency will store frequencies of element
-        int frequency[] = new int[arr.length];
-        int visited = -1;
-
-        for (int i = 0; i < arr.length; i++) {
-            int count = 1;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                    //to avoid counting same element again
-                    frequency[j] = visited;
-                }
+        int number, max;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        number = sc.nextInt();
+        int arr[] = new int[number];
+        System.out.println("Enter the elements of array: ");
+        for (int i = 0; i < number; i++) {
+            arr[i] = sc.nextInt();
+        }
+        max = arr[0];
+        for (int i = 0; i < number; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
             }
-            if (frequency[i] != visited)
-                frequency[i] = count;
         }
-        //Display the frequency of each element present in array
-        System.out.println("-----------------");
-        System.out.println("Element | Frequency");
-        System.out.println("-----------------");
-        for (int i = 0; i < arr.length; i++) {
-            if (frequency[i] != visited)
-                System.out.println("    " + arr[i] + "    |    " + frequency[i]);
-        }
-        System.out.println("-----------------");
+        System.out.println("Maximum value in the array is: " + max);
     }
 }
