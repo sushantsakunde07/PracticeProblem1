7d63f4727a7b29dafe22d7b7f2c04fe226a92c0d
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 07:53:35 +0530

    Added: smallest element in array

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class PracticeProblem {
     public static void main(String[] args) {
-        int number, max;
+        int number, min;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array: ");
+        System.out.println("Enter the number of elements in the array:");
         number = sc.nextInt();
         int arr[] = new int[number];
-        System.out.println("Enter the elements of array: ");
+        System.out.println("Enter the elements of array:");
         for (int i = 0; i < number; i++) {
             arr[i] = sc.nextInt();
         }
-        max = arr[0];
+        min = arr[0];
         for (int i = 0; i < number; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
+            if (min > arr[i]) {
+                min = arr[i];
             }
         }
-        System.out.println("Maximum value in the array is: " + max);
+        System.out.println("Minimum value in the array is: " + min);
     }
 }
