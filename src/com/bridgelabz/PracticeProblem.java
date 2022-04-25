da5a6b13f7c80e9df7199cac7a575739d3480765
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-25 07:01:51 +0530

    Added: Print elements of array

--- src/com/bridgelabz/PracticeProblem.java
+++ src/com/bridgelabz/PracticeProblem.java
@@ -1,4 +1,14 @@
 package com.bridgelabz;
 
 public class PracticeProblem {
+    public static void main(String[] args) {
+        //initialize array
+        int arr[] = {10, 20, 30, 40, 50};
+        //loop through the array by incrementing value of i
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            //System.out.print(arr[i]+ ",");
+        }
+    }
 }
+
